df8576d024e57a15912393747fd02c082c664b69
Kymage2010 <maxim07@inbox.ru>
2020-12-21 17:11:45 +0300

    21-12-20

---
+++ js/swiper_init.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+    breakpoints: {
+
+        320: {
+            slidesPerView: 1
+        },
+        660: {
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 16
+        }
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    loop: true
+});
