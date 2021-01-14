a0ad6ba14efa2125cc453275970200cc0ed1da45
Kymage2010 <maxim07@inbox.ru>
2021-01-14 10:05:30 +0300

    13-01-2021

---
+++ js/pop-up.js
@@ -0,0 +1,17 @@
+function open_popup_shop() {
+  document.getElementById("popup_shop").style.display = "flex";
+}
+
+function open_popup_to() {
+  document.getElementById("popup_to").style.display = "flex";
+}
+
+function open_popup_vin() {
+  document.getElementById("popup_vin").style.display = "flex";
+}
+
+function close_popup() {
+    document.getElementById("popup_shop").style.display = "none";
+    document.getElementById("popup_to").style.display = "none";
+    document.getElementById("popup_vin").style.display = "none";
+} 
